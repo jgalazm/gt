bde1d6e42127d35709ecf1b9578423ce98cdbea4
GALAZ MORA Jose Daniel <jose.galaz@inria.fr>
2022-12-27 11:58:30 +0100

    first step and render run (no docker yet)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,79 @@
-const { GPU } = require('gpu.js');
-const gpu = new GPU();
+const { GPU } = require("gpu.js");
+function myMod(x, y) {
+    return y * (x / y - Math.floor(x / y));
+}
+const gpu = new GPU({
+    mode: "gpu",
+    functions: [myMod]
+});
+
 
 const N = 512;
+const SIZE = N;
 
-const a = Array(512).fill().map((val1,index1)=>Array(512).fill().map((val2,index2)=>index1+index2))
 
-const b = Array(512).fill().map((val1,index1)=>Array(512).fill().map((val2,index2)=>index1+index2))
+const initialKernel = gpu
+    .createKernel(function () {
+        const x_index = this.thread.x;
+        const y_index = this.thread.y;
+        const N = this.constants.SIZE;
+        const x0 = this.constants.X0;
+        const x1 = this.constants.X1;
+        const x = x_index / (N - 1) * (x1 - x0)
+        const y = y_index / (N - 1) * (x1 - x0)
+        return Math.sin(this.thread.x * Math.PI / 2) * Math.sin(this.thread.y * Math.PI / 2)
+    })
+    .setOutput([512, 512])
+    .setPipeline(true)
+    .setConstants({
+        SIZE: N,
+        X0: 0,
+        X1: 1
+    });
+const solverKernel = gpu
+    .createKernel(function (texture) {
+        const x = this.thread.x;
+        const y = this.thread.y;
+        const SIZE = this.constants.SIZE;
+        const dx = 1 / this.constants.SIZE;
+        const dt = 0.2 * dx * dx;
+        const iminus = Math.floor(myMod(x - 1, SIZE));
+        const jminus = Math.floor(myMod(y - 1, SIZE));
+        const iplus = Math.floor(myMod(x + 1, SIZE));
+        const jplus = Math.floor(myMod(y + 1, SIZE));
 
-const multiplyMatrix = gpu.createKernel(function(a, b) {
-    let sum = 0;
-    for (let i = 0; i < 512; i++) {
-        sum += a[this.thread.y][i] * b[i][this.thread.x];
-    }
-    return sum;
-}).setOutput([512, 512]);
+        const uij = texture[this.thread.y][this.thread.x];
+        const uimj = texture[this.thread.y][iminus];
+        const uipj = texture[this.thread.y][iplus];
+        const uijm = texture[jminus][this.thread.x];
+        const uijp = texture[jplus][this.thread.x];
 
-const c = multiplyMatrix(a, b);
+        return (
+            uij +
+            (dt / dx / dx) * (uijm + uijp - 2 * uij) +
+            (dt / dx / dx) * (uimj + uipj - 2 * uij)
+        );
+    })
+    .setOutput([SIZE, SIZE])
+    .setPipeline(true)
+    .setImmutable(true)
+    .setConstants({
+        SIZE
+    });
+const render = gpu
+    .createKernel(function (texture) {
+        const val = texture[this.thread.y][this.thread.x];
+        this.color(val / this.constants.SIZE, 0, 0, 1);
+    })
+    .setOutput([N, N])
+    .setGraphical(true)
+    .setConstants({
+        SIZE: N
+    });
 
-console.log(c);
+
+const initialResult = initialKernel();
+const firstStepResult = solverKernel(initialResult);
+const initialRender = render(initialResult);
+const firstStepRender = render(firstStepResult);
+// console.log(c);
